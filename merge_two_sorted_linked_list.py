f42b2f8c9256fa3e0e7890b96e4bb8e2b9155452
gemmatruong <gemmatruong99@gmail.com>
2025-12-17 16:54:17 -0800

    feat: 2nd way to merge two sorted linked list

--- merge_two_sorted_linked_list.py
+++ merge_two_sorted_linked_list.py
@@ -46,7 +46,22 @@ class Solution:
                     current2 = current2.next
         return head
 
+        # WAY 2: shorter
+        head = ListNode()
+        node = head
 
+        while list1 and list2:
+            if list1.val <= list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        
+        node.next = list1 if list1 else list2
+
+        return head.next
 
 def main():
     sol = Solution()
